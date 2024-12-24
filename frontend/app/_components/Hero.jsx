97e2fee201c3cc6196601351714857d619a9d31d
Abhaysprasobh <abhay.s.prasobh@gmail.com>
2024-12-24 20:48:16 +0530

    Update Hero.jsx

--- frontend/app/_components/Hero.jsx
+++ frontend/app/_components/Hero.jsx
@@ -17,7 +17,7 @@ const Hero = () => {
   };
 
   return (
-    <div className="relative flex flex-col items-center justify-center h-screen bg-cover bg-center text-red-800 text-center font-serif">
+    <div className="relative flex flex-col items-center justify-center h-screen bg-red-100 bg-cover bg-center text-red-800 text-center font-serif">
       {/* Snowfall Effect */}
       {showSnow && <Snowfall />}
 
@@ -70,7 +70,7 @@ const Hero = () => {
       {/* Popup for Beautiful Moment */}
       {showPopup && (
         <div className="fixed inset-0 flex items-center justify-center bg-black bg-opacity-50 z-50">
-          <div className="bg-slate-50 rounded-lg shadow-lg p-6 relative max-w-md w-full">
+          <div className="bg-red-50 rounded-lg shadow-lg p-6 relative max-w-md w-[90vw]">
             <button
               onClick={closePopup}
               className="absolute top-2 right-2 text-gray-500 hover:text-gray-700"
